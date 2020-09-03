ea687fcbfb2a5e3208c163a7fb20a12e332b745a
simonklejnstrup <63997120+simonklejnstrup@users.noreply.github.com>
2020-09-03 11:53:53 +0200

    Added GroceryList klasse

--- src/demo/GroceryList.java
+++ src/demo/GroceryList.java
@@ -1,5 +1,21 @@
 package demo;
 
 public class GroceryList {
-    int[] orders = new int[10];
+    String[] orders = new String[10];
+
+    public void add(String order){
+        for (int i = 0; i <orders.length ; i++) {
+            if (orders[i] == null){
+                orders[i] = order;
+                break;
+            }
+        }
+
+
+
+
+    }
 }
+
+
+
