12d189d3f299b02ce7c3e6b735f697523d2f9054
simonklejnstrup <63997120+simonklejnstrup@users.noreply.github.com>
2020-09-04 10:04:36 +0200

    Ændring

--- src/demo/GroceryList.java
+++ src/demo/GroceryList.java
@@ -1,21 +1,46 @@
 package demo;
 
+import java.util.Arrays;
+
 public class GroceryList {
     GroceryItemOrder[] orders = new GroceryItemOrder[10];
 
-    public void add(GroceryItemOrder order){
+    public String add(GroceryItemOrder order){
         for (int i = 0; i <orders.length ; i++) {
             if (orders[i] == null){
                 orders[i] = order;
                 break;
             }
         }
+        boolean empty = true;
+        for (GroceryItemOrder o : orders) {
+            if (o != null) {
+                empty = false;
+                break;
+            }
+        }
+        String message = "";
+        if (empty){
+            message = "Grocerylist is full. Order not added.";
+        } else {
+            message = "Order added succesfully";
+        }
+        return message;
+    }
 
-        //METODE ER IKKE FÆRDIG
+    public double getTotalCost(){
+        double sum = 0;
+        for (GroceryItemOrder o : orders) {
+            sum += o.getCost();
+        }
+        return sum;
+    }
 
-
-
-
+    @Override
+    public String toString() {
+        return "GroceryList{" +
+                "orders=" + Arrays.toString(orders) +
+                '}';
     }
 }
 
